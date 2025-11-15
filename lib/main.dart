ba722bb7f9e2fdc81f634de195d91293b2384082
DiptiLecturer <lecturer3.amad.nsda@dipti.com.bd>
2025-11-15 16:15:52 +0600

    dashborad designed

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Modern UI Design ",
       theme: ThemeData(primarySwatch: Colors.blue),
       home: HomePage(),
@@ -24,24 +25,96 @@ class HomePage extends StatefulWidget {
 
 class _Homepage extends State<HomePage> {
   int _selectedIndex = 0; //for bottom nav bar
+
+  //for bottom nav bar
+  final List<Widget> _pages = [
+    DashboardPage(),
+    // ProfilePage(),
+    // SettingsPage()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Modern UI Design")),
-      body: Center(child: Text("Welcome")),
-      bottomNavigationBar: BottomNavigationBar(currentIndex: _selectedIndex,
-        onTap: (index){
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
           setState(() {
             _selectedIndex = index;
           });
         },
-        items:const[
-          BottomNavigationBarItem(icon: Icon(Icons.dashboard),label: "Dashboard"),
-          BottomNavigationBarItem(icon: Icon(Icons.person),label: "Person"),
-          BottomNavigationBarItem(icon: Icon(Icons.settings),label: "Settings"),
-        ]
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: "Dashboard",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Person"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
       ),
+    );
+  }
+}
 
+class SettingsPage {}
+
+class ProfilePage {}
+
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Dashboard")),
+      body: ListView(
+        padding: const EdgeInsets.all(8),
+        children: [
+          Text(
+            "Interective Cards",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 16),
+          GestureDetector(
+            onTap: () {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text("Card one Clicked")));
+            },
+            child: Card(
+              elevation: 5,
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Card one ",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text("Description for card one"),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
